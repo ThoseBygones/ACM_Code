937faed95a2a46110ae8a94dd8b7df9058517ff8
Miraslov Klose <1273789365@qq.com>
2017-05-23 14:22:05 +0800

    2017.05.23更新的代码

--- POJ/POJ 1815.cpp
+++ POJ/POJ 1815.cpp
@@ -3,122 +3,183 @@
 #include <cstring>
 #include <algorithm>
 #include <queue>
+#include <vector>
 using namespace std;
 
-#define MAXN 20
-#define MAXM 2005
-#define INF 0x3fffffff
+#define MAXN 1010
+#define MAXE 200005
+#define INF 0x3f3f3f3f
 
-struct Edge
+class Dinic
 {
-    int to;
-    int next;
-    int cap;
-} edge[MAXM];
-
-int head[MAXN]; //前向星
-int cur[MAXN];  //优化当前弧
-int level[MAXN];    //层数
-int num;    //边的数目
-int n,m;
-int st,ed;
-int ans;
-
-void addEdge(int u,int v,int w)
-{
-    edge[num].to=v;
-    edge[num].cap=w;
-    edge[num].next=head[u];
-    head[u]=num++;
-    edge[num].to=u;
-    edge[num].cap=0;
-    edge[num].next=head[v];
-    head[v]=num++;
-}
-
-void init()
-{
-    num=0;
-    memset(head,-1,sizeof(head));
-}
-
-bool bfs()
-{
-    queue <int> q;
-    memset(level,-1,sizeof(level));
-    level[st]=0;
-    q.push(st);
-    while(!q.empty())
+private:
+    int d[MAXN];
+    int head[MAXN],nxt[MAXE],cur[MAXN];
+    int g[MAXN][MAXN];  //动态关系图
+    int sg[5][MAXN]; //保存的删除某条边之前的关系图
+    struct Edge
     {
-        int u=q.front();
-        q.pop();
-        for(int i=head[u]; i!=-1; i=edge[i].next)
+        int from,to,cap,flow;
+        Edge(int from,int to,int cap,int flow):from(from),to(to),cap(cap),flow(flow) {}
+        Edge() {}
+    } e[MAXE];
+
+    vector <int> v;
+
+    int s,t,cnt;
+    int st,tm;
+    //int node;
+    int n;
+
+    bool bfs()
+    {
+        for(int i = 0; i<=2*n; i++) d[i] = -1;
+        d[s] = 0;
+        queue<int> q;
+        q.push(s);
+        while(!q.empty())
         {
-            int v=edge[i].to;
-            if(edge[i].cap>0 && level[v]==-1)   //流量不为零（是前向弧）且弧的终点未被标记过
+            int x = q.front();
+            q.pop();
+            for(int i = head[x]; ~i; i = nxt[i])
             {
-                level[v]=level[u]+1;
-                q.push(v);
+                Edge &ed = e[i];
+                if(d[ed.to]==-1&&ed.cap>ed.flow)
+                {
+                    d[ed.to] = d[x]+1;
+                    q.push(ed.to);
+                }
+            }
+        }
+        return d[t]!=-1;
+    }
+    int dfs(int x,int a)
+    {
+        if(x==t||a==0)
+            return a;
+        int flow = 0,f;
+        for(; ~cur[x]; cur[x] = nxt[cur[x]])
+        {
+            Edge &ed = e[cur[x]];
+            if(d[ed.to]==d[x]+1&&(f = dfs(ed.to,min(ed.cap-ed.flow,a)))>0)
+            {
+                flow+=f;
+                ed.flow+=f;
+                e[cur[x]^1].flow-=f;
+                a-=f;
+                if(a==0) break;
+            }
+        }
+        return flow;
+    }
+
+    void addEdge(int from,int to,int cap)
+    {
+        e[cnt] = Edge(from,to,cap,0);
+        int tmp = head[from];
+        head[from] = cnt;
+        nxt[cnt++] = tmp;
+        e[cnt] = Edge(to,from,0,0);
+        tmp = head[to];
+        head[to] = cnt;
+        nxt[cnt++] = tmp;
+    }
+public:
+    bool init()
+    {
+        scanf("%d%d%d",&n,&st,&tm);
+        s=n+st;
+        t=tm;
+        bool flag=false;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+                scanf("%d",&g[i][j]);
+        }
+        if(g[st][tm])
+            return false;
+        return true;
+    }
+
+    int maxFlow()
+    {
+        int flow = 0;
+        while(bfs())
+        {
+            for(int i = 0; i<=2*n; i++)
+                cur[i] = head[i];
+            flow+=dfs(s,INF);
+        }
+        return flow;
+    }
+
+    void buildGraph()
+    {
+        cnt = 0;
+        for(int i = 0; i<=2*n; i++)
+            head[i] = -1;
+        for(int i=1; i<=n; i++)
+        {
+            //if(st!=i && tm!=i)
+            addEdge(i,i+n,1); //拆点建边，只有拆点后的两点之间连的边能作为割边，只能被割一次
+        }
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(g[i][j] && i!=j)
+                    addEdge(i+n,j,INF);   //保证其余点不会成为割边
             }
         }
     }
-    if(level[ed]!=-1)
-        return true;
-    return false;
-}
 
-int dfs(int u,int flow)
-{
-    if(u==ed)    //把流推到了T节点（汇点）,结束
-        return flow;
-    int tempf=0;
-    for(int& i=cur[u]; i!=-1; i=edge[i].next)
+    void cutEdge(int x)
     {
-        int v=edge[i].to;
-        if(level[u]+1==level[v] && edge[i].cap>0)   //1.倒序追踪找前一个点；2.流量不为负
+        for(int i=1; i<=n; i++)
         {
-            int f=dfs(v,min(edge[i].cap,flow));  //可行流流量为整个增广路中最小的弧的增量
-            edge[i].cap-=f;
-            edge[i^1].cap+=f;
-            tempf+=f;
-            flow-=f;
-            if(!flow)
+            if(i==st || i==tm)
+                continue;
+            for(int j=1; j<=n; j++)
+            {
+                sg[0][j]=g[i][j];
+                sg[1][j]=g[j][i];
+                g[i][j]=g[j][i]=0;
+            }
+            buildGraph();
+            int temp=maxFlow();
+            //cout << temp << endl;
+            if(x>temp)
+            {
+                v.push_back(i);
+                x--;
+            }
+            else
+            {
+                for(int j=1; j<=n; j++)
+                {
+                    g[i][j]=sg[0][j];
+                    g[j][i]=sg[1][j];
+                }
+            }
+            if(x==0)
                 break;
         }
+        printf("%d\n",v.size());
+        for(int i=0; i<v.size(); i++)
+            printf("%d ",v[i]);
     }
-    return tempf;
-}
-
-void Dinic()
-{
-    while(bfs())
-    {
-        for(int i=0; i<=ed; i++)
-            cur[i]=head[i];
-        ans+=dfs(st,INF);
-    }
-}
+} dinic;
 
 int main()
 {
-    int kase=1;
-    int t;
-    scanf("%d",&t);
-    while(t--)
+    if(!dinic.init())
     {
-        init();
-        ans=0;
-        st=0;
-        ed=n;
-        scanf("%d%d",&n,&m);
-        for(int i=0; i<m; i++)
-        {
-            int x,y,c;
-            scanf("%d%d%d",&x,&y,&c);
-            addEdge(x,y,c);
-        }
-        Dinic();
-        printf("Case %d: %d\n",kase++,ans);
+        puts("NO ANSWER!");
+        return 0;
     }
+    dinic.buildGraph();
+    int ans=dinic.maxFlow();
+    //cout << ans << endl;
+    dinic.cutEdge(ans);
     return 0;
 }
