bc60822c61ed2ce207cb7cfba0650eb82ed41e67
Miraslov Klose <1273789365@qq.com>
2017-08-31 16:23:05 +0800

    2017.08.31更新的代码

--- POJ/POJ 3411.cpp
+++ POJ/POJ 3411.cpp
@@ -10,12 +10,13 @@ using namespace std;
 
 struct Road
 {
+    int b;
     int c;
     int p;
     int r;
 };
 
-vector <Road> road[MAXN][MAXN];
+vector <Road> g[MAXN];
 int dp[1<<MAXN][MAXN];
 
 int main()
@@ -24,49 +25,47 @@ int main()
     scanf("%d%d",&n,&m);
     for(int i=0; i<m; i++)
     {
-        int u,v,c,p,r;
-        scanf("%d%d%d%d%d",&u,&v,&c,&p,&r);
-        u--;
-        v--;
-        c--;
+        int u;
         Road temp;
-        temp.c=c;
-        temp.p=p;
-        temp.r=r;
-        road[u][v].push_back(temp);
+        scanf("%d%d%d%d%d",&u,&temp.b,&temp.c,&temp.p,&temp.r);
+        u--;
+        temp.b--;
+        temp.c--;
+        g[u].push_back(temp);
     }
-
     for(int i=0; i<(1<<n); i++)
         fill(dp[i],dp[i]+n,INF);
     dp[1][0]=0;
     int ans=INF;
     for(int i=0; i<(1<<n); i++) //枚举现在的所有状态
     {
-        for(int j=0; j<n; j++)  //枚举现在所在的点
+        bool flag=true;
+        while(flag)
         {
-            for(int k=0; k<n; k++)  //枚举下一个要去的点
+            flag=false;
+            for(int j=0; j<n; j++)  //枚举现在所在的点
             {
-                for(int h=0; h<road[j][k].size(); k++)
+                if(i&(1<<j))    //该点之前访问过
                 {
-                    if(j==road[j][k][h].c)
-                        dp[i|(1<<j)][j]=min(dp[i|(1<<j)][j],dp[i][j]+road[j][k][h].p);
-                }
-
-                if(road[j][k].size())   //j,k两点之间连通
-                {
-                    for(int h=0; h<road[j][k].size(); h++)
+                    for(int k=0; k<g[j].size(); k++)  //枚举下一个要去的点
                     {
-                        if(i&(1<<road[j][k][h].c))  //预pay点之前经过了
-                            dp[i][k]=min(dp[i][k],min(dp[i][j],dp[i][j]+road[j][k][h].r-road[j][k][h].p));
-                        else
-                            dp[i][k]=min(dp[i][k],dp[i][j]+road[j][k][h].r);
+                        int v=g[j][k].b;
+                        int temp=INF;
+                        if(i&(1<<g[j][k].c))
+                            temp=min(temp,dp[i][j]+g[j][k].p);  //第一种pay法
+                        temp=min(temp,dp[i][j]+g[j][k].r);  //第二种pay法
+                        if(temp<dp[i|(1<<v)][v])    //如果这次循环又找到了更省的pay法，则继续进行；否则结束
+                        {
+                            dp[i|(1<<v)][v]=temp;
+                            flag=true;
+                        }
                     }
                 }
             }
         }
-        ans=min(ans,dp[i][n-1]);
+        if(i&1) //可以不加这一句
+            ans=min(ans,dp[i][n-1]);
     }
-
     if(ans==INF)
         puts("impossible");
     else
