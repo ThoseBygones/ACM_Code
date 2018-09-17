aa482f4dfc05f950fbfb4e76073354290ae72193
Miraslov Klose <1273789365@qq.com>
2018-09-17 21:48:35 +0800

    2018.09.17更新的代码

---
+++ JSK（计蒜客）/JSK Modular Production Line.cpp
@@ -0,0 +1,214 @@
+/*
+ ********************************************************************************
+ *  Author: ThoseBygones
+ *  Version: V1.0
+ *  Date: 2018-09-15
+ *  Subject: ACM-ICPC
+ *  Language: C/C++11
+ *  OJ: 计蒜客（JSK）
+ *  Algorithm:
+ ********************************************************************************
+ *  Algo-Description:
+ ********************************************************************************
+ */
+
+//#include <bits/stdc++.h>  //For CodeForces only
+
+//#pragma comment(linker,"/STACK:102400000,102400000")
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <climits>
+#include <ctime>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <list>
+#include <map>
+#include <string>
+#include <sstream>
+#include <set>
+#include <stack>
+#include <queue>
+using namespace std;
+template<class T> inline T sqr(T x)
+{
+    return x * x;
+}
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<LL, LL> PLL;
+typedef pair<LL, int> PLI;
+typedef pair<LD, LD> PDD;
+#define MP make_pair
+#define PB push_back
+#define sz(x) ((int)(x).size())
+#define clr(ar,val) memset(ar, val, sizeof(ar))
+#define istr stringstream
+#define FOR(i,n) for(int i=0;i<(n);++i)
+#define forIt(mp,it) for(__typeof(mp.begin()) it = mp.begin();it!=mp.end();it++)
+const double EPS = 1e-6;
+const int INF = 0x3fffffff;
+const LL LINF = INF * 1ll * INF;
+const double PI = acos(-1.0);
+
+#define lson l,mid,rt<<1
+#define rson mid+1,r,rt<<1|1
+#define lowbit(u) (u&(-u))
+#define MAXN 200010
+#define MAXE 1000010
+
+class MCMF
+{
+public:
+    typedef int CostType;
+    struct Edge
+    {
+        int from,to,cap,flow;
+        CostType cost;
+        Edge(int from,int to,int cap,int flow,CostType cost):from(from),to(to),cap(cap),flow(flow),cost(cost) {}
+        Edge() {}
+    } e[MAXE];
+
+    int head[MAXN],nxt[MAXE],a[MAXN],p[MAXN];
+    bool inq[MAXN];
+    CostType d[MAXN];
+    int cnt,s,t;
+    int node;
+    int n,m,k;
+
+    int u[MAXN],v[MAXN],c[MAXN];
+    int tmp[MAXN];
+
+    void init(int n,int k,int m)
+    {
+        cnt = 0;
+        this -> n = n;
+        this -> k = k;
+        this -> m = m;
+    }
+
+    void addEdge(int from,int to,int cap,CostType cost)
+    {
+        e[cnt] = Edge(from,to,cap,0,cost);
+        int tmp = head[from];
+        head[from] = cnt;
+        nxt[cnt++] = tmp;
+        e[cnt] = Edge(to,from,0,0,-cost);
+        tmp = head[to];
+        head[to] = cnt;
+        nxt[cnt++] = tmp;
+    }
+
+    bool spfa(int &flow,CostType &cost)
+    {
+        for(int i = 0; i<=node; i++)
+        {
+            d[i] = INF;
+            inq[i] = false;
+        }
+        d[s] = 0,inq[s] = true,p[s] = 0,a[s] = INF;
+        queue<int> q;
+        q.push(s);
+        while(!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+            inq[u] = false;
+            for(int i = head[u]; ~i; i = nxt[i])
+            {
+                Edge &ed = e[i];
+                if(ed.cap>ed.flow&&d[ed.to]>d[u]+ed.cost)
+                {
+                    d[ed.to] = d[u]+ed.cost;
+                    p[ed.to] = i;
+                    a[ed.to] = min(a[u],ed.cap-ed.flow);
+                    if(!inq[ed.to])
+                    {
+                        inq[ed.to] = true;
+                        q.push(ed.to);
+                    }
+                }
+            }
+        }
+        if(d[t]==INF)
+            return false;
+        flow+=a[t];
+        cost+=d[t]*a[t];
+        int u = t;
+        while(u!=s)
+        {
+            e[p[u]].flow+=a[t];
+            e[p[u]^1].flow-=a[t];
+            u = e[p[u]].from;
+        }
+        return true;
+    }
+
+    CostType maxFlow()
+    {
+        int flow = 0;
+        CostType cost = 0;
+        while(spfa(flow,cost));
+        //return flow;
+        return cost;
+    }
+
+    void buildGraph()
+    {
+        memset(head,-1,sizeof(head));
+        int num=0;
+        for(int i=1; i<=m; i++)
+        {
+            scanf("%d%d%d",&u[i],&v[i],&c[i]);
+            tmp[num++]=u[i];
+            tmp[num++]=++v[i];
+        }
+        sort(tmp,tmp+num);
+        int tot=unique(tmp,tmp+num)-tmp;
+        for(int i=1; i<tot; i++)
+            addEdge(i-1,i,k,0);
+        for(int i=1; i<=m; i++)
+        {
+            int l=lower_bound(tmp,tmp+tot,u[i])-tmp;
+            int r=lower_bound(tmp,tmp+tot,v[i])-tmp;
+            addEdge(l,r,1,-c[i]);
+        }
+        s=tot,t=tot+1;
+        node=t;
+        addEdge(s,0,k,0);
+        addEdge(tot-1,t,k,0);
+    }
+
+    void solve()
+    {
+        buildGraph();
+        int ans=maxFlow();
+        printf("%d\n",-ans);
+    }
+} mcmf;
+
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    while(T--)
+    {
+        int n,k,m;
+        scanf("%d%d%d",&n,&k,&m);
+        mcmf.init(n,k,m);
+        mcmf.solve();
+    }
+    return 0;
+}
