56d7a293b20a6d3ea9d62ef1c0a7d491637c890a
ThoseBygones <1273789365@qq.com>
2021-01-01 23:24:07 +0800

    2021.01.01.UPD

---
+++ POJ/POJ 2348.cpp
@@ -0,0 +1,85 @@
+/*
+ ********************************************************************************
+ *  Author: ThoseBygones
+ *  Version: V1.0
+ *  Date: 2021-01-01
+ *  Subject: ACM-ICPC
+ *  Language: C/C++14
+ *  OJ: POJ
+ *  Algorithm: ²©ÞÄ
+ ********************************************************************************
+ *  Algo-Description:
+ ********************************************************************************
+ */
+
+//#pragma comment(linker,"/STACK:102400000,102400000")
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <climits>
+#include <ctime>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <list>
+#include <map>
+#include <string>
+#include <sstream>
+#include <set>
+#include <stack>
+#include <queue>
+//#include <bits/stdc++.h>
+using namespace std;
+
+template<class T> inline T sqr(T x) {return x * x;}
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<LL, LL> PLL;
+typedef pair<LL, int> PLI;
+typedef pair<LD, LD> PDD;
+#define MP make_pair
+#define PB push_back
+#define sz(x) ((int)(x).size())
+const double EPS = 1e-6;
+const int INF = 0x3fffffff;
+const LL LINF = INF * 1ll * INF;
+const double PI = acos(-1.0);
+
+#define lson l,mid,rt<<1
+#define rson mid+1,r,rt<<1|1
+#define lowbit(u) (u&(-u))
+
+int main()
+{
+    int a, b;
+    while(~scanf("%d%d", &a, &b))
+    {
+        if(a == 0 && b == 0)
+            break;
+        bool flag = true;
+        while(true)
+        {
+            if(a > b)
+                swap(a, b);
+            if(b % a == 0)
+                break;
+            if(b - a > a)
+                break;
+            b -= a;
+            flag ^= 1;
+        }
+        puts(flag ? "Stan wins" : "Ollie wins");
+    }
+    return 0;
+}
