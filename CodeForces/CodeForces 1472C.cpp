e50838b7a98a8906f929d7e5e5da89e12d7981a5
ThoseBygones <1273789365@qq.com>
2021-05-01 22:22:38 +0800

    2021.05.01

---
+++ CodeForces/CodeForces 1472C.cpp
@@ -0,0 +1,73 @@
+/*
+ ********************************************************************************
+ *  Author: ThoseBygones
+ *  Version: V1.0
+ *  Date: 2021-04-30
+ *  Subject: ACM-ICPC
+ *  Language: C/C++14
+ *  OJ: CodeForces
+ *  Algorithm:
+ ********************************************************************************
+ *  Algo-Description:
+ ********************************************************************************
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T> inline T sqr(T x) {return x * x;}
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<LL, LL> PLL;
+typedef pair<LL, int> PLI;
+typedef pair<LD, LD> PDD;
+#define MP make_pair
+#define PB push_back
+#define sz(x) ((int)(x).size())
+const double EPS = 1e-6;
+const int INF = 0x3fffffff;
+const LL LINF = INF * 1ll * INF;
+const double PI = acos(-1.0);
+
+#define lson l,mid,rt<<1
+#define rson mid+1,r,rt<<1|1
+#define lowbit(u) (u&(-u))
+
+const int MAXN = 200005;
+
+int a[MAXN];
+int vis[MAXN];
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int n;
+        scanf("%d", &n);
+        for(int i = 1; i <= n; i++)
+        {
+            scanf("%d", &a[i]);
+            vis[i] = false;
+        }
+        int ans = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            int sum = 0, pos = i;
+            while(pos <= n && !vis[pos])
+            {
+                sum += a[pos];
+                vis[pos] = true;
+                pos += a[pos];
+                //printf("pos: %d, sum: %d\n", pos, sum);
+            }
+            ans = max(ans, sum);
+        }
+        printf("%d\n", ans);
+    }
+    return 0;
+}
---
+++ CodeForces/CodeForces 1472D.cpp
@@ -0,0 +1,79 @@
+/*
+ ********************************************************************************
+ *  Author: ThoseBygones
+ *  Version: V1.0
+ *  Date: 2021-04-30
+ *  Subject: ACM-ICPC
+ *  Language: C/C++14
+ *  OJ: CodeForces
+ *  Algorithm: 贪心 + 博弈
+ ********************************************************************************
+ *  Algo-Description:
+ ********************************************************************************
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T> inline T sqr(T x)
+{
+    return x * x;
+}
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<LL, LL> PLL;
+typedef pair<LL, int> PLI;
+typedef pair<LD, LD> PDD;
+#define MP make_pair
+#define PB push_back
+#define sz(x) ((int)(x).size())
+const double EPS = 1e-6;
+const int INF = 0x3fffffff;
+const LL LINF = INF * 1ll * INF;
+const double PI = acos(-1.0);
+
+#define lson l,mid,rt<<1
+#define rson mid+1,r,rt<<1|1
+#define lowbit(u) (u&(-u))
+
+const int MAXN = 200005;
+
+int a[MAXN];
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int n;
+        scanf("%d", &n);
+        for(int i = 0; i < n; i++)
+            scanf("%d", &a[i]);
+        sort(a, a + n, greater<int>());
+        LL alice = 0, bob = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(i & 1)   //轮到Bob拿
+            {
+                if(a[i] & 1)    //奇数数字
+                    bob += a[i];
+            }
+            else    //轮到Alice拿
+            {
+                if(a[i] % 2 == 0)   //偶数数字
+                    alice += a[i];
+            }
+        }
+        if(alice > bob)
+            puts("Alice");
+        else if(alice == bob)
+            puts("Tie");
+        else
+            puts("Bob");
+    }
+    return 0;
+}
