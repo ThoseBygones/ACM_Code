e2ae94b1616951a78f9361985171a80e17b2e9d8
ThoseBygones <1273789365@qq.com>
2023-09-23 01:03:48 +0800

    2023.09.23.UPD

---
+++ CodeForces/CodeForces 1521A.cpp
@@ -0,0 +1,56 @@
+/*
+ ********************************************************************************
+ *  Author: ThoseBygones
+ *  Version: V1.0
+ *  Date: 2023-09-22
+ *  Subject: ACM-ICPC
+ *  Language: C/C++14
+ *  OJ: CodeForces
+ *  Algorithm:
+ ********************************************************************************
+ *  Algo-Description:
+ ********************************************************************************
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T> inline T sqr(T x) {return x * x;}
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<LL, LL> PLL;
+typedef pair<LL, int> PLI;
+typedef pair<LD, LD> PDD;
+#define MP make_pair
+#define PB push_back
+#define sz(x) ((int)(x).size())
+const double EPS = 1e-6;
+const int INF = 0x3fffffff;
+const LL LINF = INF * 1ll * INF;
+const double PI = acos(-1.0);
+
+#define lson l,mid,rt<<1
+#define rson mid+1,r,rt<<1|1
+#define lowbit(u) (u&(-u))
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        LL a, b;
+        scanf("%lld%lld", &a, &b);
+        if(b == 1)
+            puts("NO");
+        else
+        {
+            puts("YES");
+            printf("%lld %lld %lld\n", a, a * b, a * (b + 1));
+        }
+    }
+    return 0;
+}
